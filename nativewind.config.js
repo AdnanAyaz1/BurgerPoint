acb9200e254060db26b34ea67f597a68f4669080
Adnan Ayaz <adnanayazkhan479@gmail.com>
2025-09-01 23:50:18 +0500

    fix font issues

---
+++ nativewind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ["Quicksand-Regular"],
+        "quicksand-bold": ["Quicksand-Bold"],
+        "quicksand-semibold": ["Quicksand-SemiBold"],
+        "quicksand-light": ["Quicksand-Light"],
+        "quicksand-medium": ["Quicksand-Medium"],
+      },
+    },
+  },
+};
