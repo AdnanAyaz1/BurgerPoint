33a73483c64c13af89287de26529016036a4870b
Adnan Ayaz <adnanayazkhan479@gmail.com>
2025-09-02 02:46:53 +0500

    fixed keyboard ui issues

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,32 +1,37 @@
-import {View,KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from 'react-native'
-import {Slot} from "expo-router";
-import {images} from "@/constants";
+import { images } from "@/constants";
+import { Slot } from "expo-router";
+import { Dimensions, Image, ImageBackground, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function AuthLayout() {
+  const insets = useSafeAreaInsets();
+  const maxHeight = Dimensions.get("window").height - insets.bottom;
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    <KeyboardAwareScrollView
+      style={{ flex: 1, backgroundColor: "#ffffff" }}
+      contentContainerStyle={{ maxHeight: maxHeight }}
+      enableOnAndroid={true}
+      extraScrollHeight={50}
+      keyboardShouldPersistTaps="handled"
+      showsVerticalScrollIndicator={false}
     >
-      <ScrollView
-        className="h-full bg-white"
-        keyboardShouldPersistTaps="handled"
+      <View
+        className="relative w-full"
+        style={{ height: Dimensions.get("window").height / 2.25 }}
       >
-        <View
-          className="relative w-full"
-          style={{ height: Dimensions.get("screen").height / 2.25 }}
-        >
-          <ImageBackground
-            source={images.loginGraphic}
-            className="rounded-b-lg size-full"
-            resizeMode="stretch"
-          />
-          <Image
-            source={images.logo}
-            className="absolute z-10 self-center size-48 -bottom-16"
-          />
-        </View>
-        <Slot />
-      </ScrollView>
-    </KeyboardAvoidingView>
+        <ImageBackground
+          source={images.loginGraphic}
+          className="rounded-b-lg size-full"
+          resizeMode="stretch"
+        />
+        <Image
+          source={images.logo}
+          className="absolute z-10 self-center size-48 -bottom-16"
+        />
+      </View>
+
+      <Slot />
+    </KeyboardAwareScrollView>
   );
 }
