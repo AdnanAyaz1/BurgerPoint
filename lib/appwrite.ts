2942d87e43daa3b227092f3d94df9c51a5f86f56
Adnan Ayaz <adnanayazkhan479@gmail.com>
2025-09-02 01:08:12 +0500

    added missing database id

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,5 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!,
+  databaseId : process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!
 };
