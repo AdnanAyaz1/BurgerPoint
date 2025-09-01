7cc019b244ea8ffcacc149bbd897cb89455b481a
Adnan Ayaz <adnanayazkhan479@gmail.com>
2025-09-02 02:12:52 +0500

    updated global css file

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const isAuthenticated = false; 
+
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
   return (
     <View className="items-center justify-center flex-1 bg-white">
       <Text className="text-5xl text-center text-blue-500 font-quicksand-bold">
