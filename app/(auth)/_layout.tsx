c23db326d8d2615bb6ff2e30ebfc2c1d416477bd
Adnan Ayaz <adnanayazkhan479@gmail.com>
2025-09-02 02:03:35 +0500

    implemented auth screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { Text, View } from "react-native";
+import {View,KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from 'react-native'
+import {Slot} from "expo-router";
+import {images} from "@/constants";
 
-export default function Layout() {
+export default function AuthLayout() {
   return (
-    <View>
-      <Text>Layout</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="h-full bg-white"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="relative w-full"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="rounded-b-lg size-full"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="absolute z-10 self-center size-48 -bottom-16"
+          />
+        </View>
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
